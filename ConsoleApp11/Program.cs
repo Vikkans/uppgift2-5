06720a78be6a0535ecfce808b331a92504ff57d8
Vikkans <Jakobsenviktor6@gmail.com>
2023-09-15 13:28:38 +0200

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Skriv in din beräkning med två tal som infattar en decimal");
+            string helaberäkning = Console.ReadLine();
+
+            int gångerindex = helaberäkning.IndexOf("*");
+
+            string förstatalet = helaberäkning[..gångerindex];
+            string andratalet = helaberäkning[(gångerindex + 1)..];
+
+            double x = double.Parse(andratalet);
+            double y = double.Parse(förstatalet);
+
+            double sum = y * x;
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+             
+
+        
+        
+        }
+      
+
+
+    }
+
+
+
+}
